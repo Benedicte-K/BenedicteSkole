75c498e169a49d81d71374b9ddcc479a19379ea6
Benedicte Karlsen <benne_jenta@hotmail.no>
2024-12-10 10:00:17 +0100

    Delete rps directory

--- rps/lib/output.mjs
+++
@@ -1,7 +0,0 @@
-import { ANSI } from './ansi.mjs';
-
-function print(text, color = ANSI.COLOR.WHITE) {
-    console.log(color, text, ANSI.RESET)
-}
-
-export { print }
--- rps/lib/random.mjs
+++
@@ -1,8 +0,0 @@
-function getRandomItemFromArray(array) {
-    let max = array.length - 1;
-    let rndIndex = Math.round(Math.random() * max);
-    return array[rndIndex];
-}
-
-
-export { getRandomItemFromArray }
